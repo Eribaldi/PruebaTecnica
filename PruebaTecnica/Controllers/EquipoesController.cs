28eda7986141b5a177777eea5dea9fd3bd72cca1
Pedro Eribaldi Blázquez González <pedro.e.blazquez.g@gmail.com>
2021-08-26 13:04:23 -0400

    Modificacion de Vistas

---
+++ PruebaTecnica/Controllers/EquipoesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Data;
+using PruebaTecnica.Models;
+
+namespace PruebaTecnica.Controllers
+{
+    public class EquipoesController : Controller
+    {
+        private readonly EquiposContext _context;
+
+        public EquipoesController(EquiposContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipoes
+        public async Task<IActionResult> Index()
+        {
+            var equiposContext = _context.Equipos.Include(e => e.Estado);
+            return View(await equiposContext.ToListAsync());
+        }
+
+        // GET: Equipoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipo = await _context.Equipos
+                .Include(e => e.Estado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Create
+        public IActionResult Create()
+        {
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id");
+            return View();
+        }
+
+        // POST: Equipoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Pais,EstadoId,FechaCreacion")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", equipo.EstadoId);
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipo = await _context.Equipos.FindAsync(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", equipo.EstadoId);
+            return View(equipo);
+        }
+
+        // POST: Equipoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Pais,EstadoId,FechaCreacion")] Equipo equipo)
+        {
+            if (id != equipo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipoExists(equipo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", equipo.EstadoId);
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipo = await _context.Equipos
+                .Include(e => e.Estado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipo);
+        }
+
+        // POST: Equipoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipo = await _context.Equipos.FindAsync(id);
+            _context.Equipos.Remove(equipo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipoExists(int id)
+        {
+            return _context.Equipos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PruebaTecnica/Controllers/JugadorsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Data;
+using PruebaTecnica.Models;
+
+namespace PruebaTecnica.Controllers
+{
+    public class JugadorsController : Controller
+    {
+        private readonly EquiposContext _context;
+
+        public JugadorsController(EquiposContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jugadors
+        public async Task<IActionResult> Index()
+        {
+            var equiposContext = _context.Jugadores.Include(j => j.Equipo).Include(j => j.Estado);
+            return View(await equiposContext.ToListAsync());
+        }
+
+        // GET: Jugadors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jugador = await _context.Jugadores
+                .Include(j => j.Equipo)
+                .Include(j => j.Estado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+
+            return View(jugador);
+        }
+
+        // GET: Jugadors/Create
+        public IActionResult Create()
+        {
+            ViewData["EquipoId"] = new SelectList(_context.Equipos, "Id", "Id");
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id");
+            return View();
+        }
+
+        // POST: Jugadors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Apellido,FechaNacimiento,Pasaporte,Direccion,Sexo,EquipoId,EstadoId")] Jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jugador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EquipoId"] = new SelectList(_context.Equipos, "Id", "Id", jugador.EquipoId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", jugador.EstadoId);
+            return View(jugador);
+        }
+
+        // GET: Jugadors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jugador = await _context.Jugadores.FindAsync(id);
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+            ViewData["EquipoId"] = new SelectList(_context.Equipos, "Id", "Id", jugador.EquipoId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", jugador.EstadoId);
+            return View(jugador);
+        }
+
+        // POST: Jugadors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Apellido,FechaNacimiento,Pasaporte,Direccion,Sexo,EquipoId,EstadoId")] Jugador jugador)
+        {
+            if (id != jugador.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jugador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JugadorExists(jugador.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EquipoId"] = new SelectList(_context.Equipos, "Id", "Id", jugador.EquipoId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", jugador.EstadoId);
+            return View(jugador);
+        }
+
+        // GET: Jugadors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jugador = await _context.Jugadores
+                .Include(j => j.Equipo)
+                .Include(j => j.Estado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+
+            return View(jugador);
+        }
+
+        // POST: Jugadors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jugador = await _context.Jugadores.FindAsync(id);
+            _context.Jugadores.Remove(jugador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JugadorExists(int id)
+        {
+            return _context.Jugadores.Any(e => e.Id == id);
+        }
+    }
+}
